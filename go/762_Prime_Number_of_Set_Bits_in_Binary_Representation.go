d4a77316f92c520e7394262c4f02e9c2bd2b4ae1
Hang Zhang <hangzhang.h@gmail.com>
2022-02-24 16:25:50 +0800

    [go] 762. Prime Number of Set Bits in Binary Representation

---
+++ go/762_Prime_Number_of_Set_Bits_in_Binary_Representation.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+)
+
+func countPrimeSetBits(left int, right int) int {
+	bitmapPrime := 0b10100010100010101100	// 2,3,5,7,11,13,17,19 bit set
+	count := 0
+	for i := left; i <= right; i++ {
+		nBits := 0
+		for n := i; n > 0; n /= 2 {
+			nBits += n % 2
+		}
+		if (bitmapPrime >> nBits ) & 1 == 1 {
+			count++
+		}
+	}
+	return count
+}
+
+// func main() {
+// 	fmt.Printf("LeetCode 762. Prime Number of Set Bits in Binary Representation ...\n\n")
+
+// 	type args struct {
+// 		left  int
+// 		right int
+// 	}
+// 	tests := []struct {
+// 		args args
+// 		want int
+// 	}{
+// 		{
+// 			args: args{
+// 				left:  6,
+// 				right: 10,
+// 			},
+// 			want: 4,
+// 		},
+// 		{
+// 			args: args{
+// 				left:  10,
+// 				right: 15,
+// 			},
+// 			want: 5,
+// 		},
+// 	}
+// 	for _, tt := range tests {
+// 		r := countPrimeSetBits(tt.args.left, tt.args.right)
+// 		fmt.Println(r)
+// 	}
+
+// }
